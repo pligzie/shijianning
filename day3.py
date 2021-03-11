ed2138e9e2caff2a76b26bf8f21f3040c46d670a
pligzie <80317370+pligzie@users.noreply.github.com>
2021-03-11 16:28:03 +0800

    Add files via upload

---
+++ day3.py
@@ -0,0 +1,60 @@
+# a = ["北京","上海","广东"]
+# a.append("天津")
+# a.append("重庆")
+# a.append("河北")
+# a.append("山西")
+# a.append("辽宁")
+# a.append("吉林")
+# a.append("黑龙江")
+# a.append("江苏")
+# a.append("浙江")
+# a.append("安徽")
+# a.append("福建")
+# a.append("江西")
+# a.append("山东")
+# a.append("河南")
+# a.append("湖北")
+# a.append("湖南")
+# a.append("海南")
+# a.append("四川")
+# a.append("贵州")
+# a.append("云南")
+# a.append("陕西")
+# a.append("甘肃")
+# a.append("青海")
+# a.append("台湾")
+# a.append("内蒙古")
+# a.append("广西")
+# a.append("西藏")
+# a.append("宁夏")
+# a.append("新疆")
+# a.append("香港")
+# a.append("澳门")
+# a.remove("广东")
+# a.insert(1,"广东")
+# print(a)
+
+#
+# gdp=[36710.36,35427.10,29863.23,29667.39,27665.36,27650.45,27620.38,25369.20]
+# a = 0
+# z=0
+# while a <= 7:
+#     z=z+gdp[a]
+#     a = a + 1
+# print(round(z,2),round(z/8,2))
+#
+
+# a = [1,5,21,30,15,9,30,24]
+# b = 0
+# c = 0
+# while b<=7:
+#     if a[b] % 5 == 0:
+#         c = c+a[b]
+#     b = b + 1
+# print(c)
+#
+# b = [1,2,3,4,5,6,7,8,9]
+# a = list(reversed(b))
+# print(a)
+
+List = [1,4,7,5,8,2,1,3,4,5,9,7,6,1,10]
